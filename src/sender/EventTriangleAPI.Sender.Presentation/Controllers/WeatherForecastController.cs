3e70797594af04003c153061a5f4b95ac7567816
kolosovpetro <kolosovp94@gmail.com>
2023-04-04 19:51:05 +0200

    work in progress

--- src/sender/EventTriangleAPI.Sender.Presentation/Controllers/WeatherForecastController.cs
+++ src/sender/EventTriangleAPI.Sender.Presentation/Controllers/WeatherForecastController.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
 
 namespace EventTriangleAPI.Sender.Presentation.Controllers;
 
 [ApiController]
 [Route("[controller]")]
+[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
@@ -18,8 +21,22 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [Authorize(Roles = "User")]
+    [HttpGet("user")]
+    public IEnumerable<WeatherForecast> GetForUser()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpGet("admin")]
+    public IEnumerable<WeatherForecast> GetForAdmin()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
