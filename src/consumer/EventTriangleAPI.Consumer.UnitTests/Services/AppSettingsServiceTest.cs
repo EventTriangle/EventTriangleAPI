40bcccbb7b03508212d89c5d5201501f722e10a9
kami no me <kudryashov.kd@gmail.com>
2023-04-06 20:22:57 +0400

    Add unit tests for AppSettingsService

---
+++ src/consumer/EventTriangleAPI.Consumer.UnitTests/Services/AppSettingsServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using EventTriangleAPI.Shared.Application.Services;
+using FluentAssertions;
+using Xunit;
+
+namespace EventTriangleAPI.Consumer.UnitTests.Services;
+
+public class AppSettingsServiceTest
+{
+    [Fact]
+    public void Test()
+    {
+        var appSettingsService = new AppSettingsService();
+
+        var path = appSettingsService.GetAppSettingsPathConsumer();
+
+        File.Exists(path).Should().BeTrue();
+    }
+}
---
+++ src/sender/EventTriangleAPI.Sender.UnitTests/Services/AppSettingsServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using EventTriangleAPI.Shared.Application.Services;
+using FluentAssertions;
+using Xunit;
+
+namespace EventTriangleAPI.Sender.UnitTests.Services;
+
+public class AppSettingsServiceTest
+{
+    [Fact]
+    public void Test()
+    {
+        var appSettingsService = new AppSettingsService();
+
+        var path = appSettingsService.GetAppSettingsPathSender();
+
+        File.Exists(path).Should().BeTrue();
+    }
+}
