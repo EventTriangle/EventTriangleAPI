ab9467dcc0ea25255806fc91b834eca24b4bf50d
kami no me <kudryashov.kd@gmail.com>
2023-04-06 19:56:42 +0400

    Add AppSettingsService

---
+++ src/shared/EventTriangleAPI.Shared.Application/Services/AppSettingsService.cs
@@ -0,0 +1,30 @@
+﻿namespace EventTriangleAPI.Shared.Application.Services;
+
+public class AppSettingsService
+{
+    private const string AppSettingsPathSender = "../../sender/EventTriangleAPI.Sender.Presentation/appsettings.json";
+
+    private const string AppSettingsPathConsumer = "../../consumer/EventTriangleAPI.Consumer.Presentation/appsettings.json";
+    
+    public string GetAppSettingsPathSender()
+    {
+        var workingDirectory = Environment.CurrentDirectory;
+        
+        var projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+        
+        var path = Path.Combine(projectDirectory, AppSettingsPathSender);
+        
+        return path;
+    }
+    
+    public string GetAppSettingsPathConsumer()
+    {
+        var workingDirectory = Environment.CurrentDirectory;
+        
+        var projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+        
+        var path = Path.Combine(projectDirectory, AppSettingsPathConsumer);
+        
+        return path;
+    }
+}
