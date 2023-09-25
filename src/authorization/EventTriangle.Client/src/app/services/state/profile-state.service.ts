d3795f00b8f5ca28df12ceeb4e4a16cc18857fcc
Ketteiteki <kudryashov.kd@gmail.com>
2023-09-25 23:07:08 +0400

    add skeleton loaders and comment them

--- src/authorization/EventTriangle.Client/src/app/services/state/profile-state.service.ts
+++ src/authorization/EventTriangle.Client/src/app/services/state/profile-state.service.ts
@@ -1,9 +1,5 @@
 import {Injectable} from "@angular/core";
 import {UserDto} from "../../types/models/consumer/UserDto";
-import {WalletDto} from "../../types/models/consumer/WalletDto";
-import {UserRole} from "../../types/enums/UserRole";
-import {UserStatus} from "../../types/enums/UserStatus";
-import { BehaviorSubject } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -12,22 +8,5 @@ export class ProfileStateService {
   public wasAuthenticationCheck = false;
   public isAuthenticated = false;
 
-  public user: UserDto;
-
-  constructor() {
-    const emptyWallet : WalletDto = {
-      id: '',
-      balance: 0,
-      lastTransactionId: ''
-    };
-
-    this.user = {
-      id: '',
-      email: '',
-      userRole: UserRole.User,
-      userStatus: UserStatus.Active,
-      walletId: '',
-      wallet: emptyWallet
-    };
-  }
+  public user: UserDto | null = null;
 }
