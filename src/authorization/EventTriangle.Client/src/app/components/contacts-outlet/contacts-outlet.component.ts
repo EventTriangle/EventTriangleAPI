1ca6f5918a9e069ed2f04c44d3b2fdcdc026e547
Ketteiteki <kudryashov.kd@gmail.com>
2023-10-04 00:49:33 +0400

    partially implement user page | change animation

--- src/authorization/EventTriangle.Client/src/app/components/contacts-outlet/contacts-outlet.component.ts
+++ src/authorization/EventTriangle.Client/src/app/components/contacts-outlet/contacts-outlet.component.ts
@@ -1,13 +1,14 @@
-import {Component, OnInit} from '@angular/core';
-import {animate, group, query, stagger, style, transition, trigger} from "@angular/animations";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {animate, animateChild, query, stagger, style, transition, trigger} from "@angular/animations";
 import {ContactsStateService} from "../../services/state/contacts-state.service";
 import {ProfileStateService} from "../../services/state/profile-state.service";
 import {
   BehaviorSubject,
-  debounceTime, filter
+  debounceTime, filter, Subscription
 } from "rxjs";
 import {TextService} from "../../services/common/text.service";
 import {IContactDto} from "../../types/interfaces/consumer/IContactDto";
+import {NavigationEnd, NavigationStart, Router} from "@angular/router";
 
 @Component({
   selector: 'app-contacts-outlet',
@@ -20,6 +21,9 @@ import {IContactDto} from "../../types/interfaces/consumer/IContactDto";
         query(':enter', stagger('50ms', [
           animate('150ms', style({ marginTop: 0, opacity: 1 }))
         ]), { optional: true })
+      ]),
+      transition(":leave", [
+          query("@contactAnimation", animateChild({duration: "0.25s"}), {optional: true})
       ])
     ]),
     trigger("contactAnimation", [
@@ -29,18 +33,31 @@ import {IContactDto} from "../../types/interfaces/consumer/IContactDto";
     ])
   ]
 })
-export class ContactsOutletComponent implements OnInit{
+export class ContactsOutletComponent implements OnInit, OnDestroy{
+
   //observable
   public contacts$ = this._contactsStateService.contacts$;
   public searchedContacts$ = this._contactsStateService.searchedContacts$;
 
   public searchText$ = new BehaviorSubject<string>("");
 
+  //subscriptions
+  public routerSubscription: Subscription;
+
+  //common
+  public contactListAnimContinues = false;
+  public searchedContactListAnimContinues = false;
+  public isAnimationAllowed = true;
+
   constructor(
       protected _contactsStateService: ContactsStateService,
       protected _profileStateService: ProfileStateService,
-      protected _textService: TextService
-  ) {}
+      protected _textService: TextService,
+      protected _router: Router
+  ) {
+    this.routerSubscription = this._router.events.pipe(filter((event): event is NavigationStart => event instanceof NavigationStart))
+        .subscribe(x => this.isAnimationAllowed = false)
+  }
 
   async ngOnInit() {
     if (!this._profileStateService.isAuthenticated) return;
@@ -58,6 +75,10 @@ export class ContactsOutletComponent implements OnInit{
         .subscribe(_ => this._contactsStateService.clearSearchContacts())
   }
 
+  ngOnDestroy() {
+    this.routerSubscription.unsubscribe();
+  }
+
   //events
   async onClickAddContactHandler(contactId: string) {
     await this._contactsStateService.addContactAsync(contactId);
@@ -71,4 +92,7 @@ export class ContactsOutletComponent implements OnInit{
   identifyContactDto(index: number, item: IContactDto) {
     return item.contactId + item.userId;
   }
+
+  setContactListAnimContinues = (v: boolean) => this.contactListAnimContinues = v;
+  setSearchedContactListAnimContinues = (v: boolean) => this.searchedContactListAnimContinues = v;
 }
