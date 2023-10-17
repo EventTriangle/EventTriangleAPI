a011f35e9e71bb198be04393eef02a9f587fa1c3
ketteiteki <kudryashov.kd@gmail.com>
2023-10-17 05:59:13 +0400

    add nav link skeletons | hide some nav link for user role | several changes

--- src/authorization/EventTriangle.Client/src/app/components/navbar/navbar.component.ts
+++ src/authorization/EventTriangle.Client/src/app/components/navbar/navbar.component.ts
@@ -1,28 +1,35 @@
 import { Component } from '@angular/core';
 import {animate, style, transition, trigger} from "@angular/animations";
 import { AuthorizationApiService } from 'src/app/services/api/authorization-api.service';
+import {ProfileStateService} from "../../services/state/profile-state.service";
+import {UserRole} from "../../types/enums/UserRole";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
   animations: [
-    trigger("navbarAnimation", [
+    trigger("navbarLinkAnimation", [
       transition(":enter", [
-        style({opacity: 0}),
-        animate(".3s", style({opacity: 1}))
+        style({transform: 'translateY(-10px)', opacity: 0}),
+        animate(".3s", style({transform: 'translateY(0px)', opacity: 1}))
       ])
     ])
   ]
 })
 export class NavbarComponent {
-  public authorizationApiService;
+  //observable
+  public user$ = this._profileStateService.user$;
 
-  constructor(authorizationApiService: AuthorizationApiService) {
-    this.authorizationApiService = authorizationApiService;
-  };
+  //types
+  public UserRole = UserRole;
+
+  constructor(
+      private _authorizationApiService: AuthorizationApiService,
+      protected _profileStateService: ProfileStateService
+      ) {}
 
   async getLogout() {
-    window.location.href = this.authorizationApiService.getLogoutPathForRedirection();
+    window.location.href = this._authorizationApiService.getLogoutPathForRedirection();
   }
 }
