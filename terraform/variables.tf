1e3aa636cb1e4dfc9c6fb12e2d42210dc33ef94e
kolosovpetro <kolosovp94@gmail.com>
2024-12-11 01:34:59 +0100

    tech debt terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,64 +1,84 @@
 variable "resource_group_name" {
-  type        = string
-  description = "Resource group name"
+    type        = string
+    description = "Resource group name"
 }
 
 variable "resource_group_location" {
-  type        = string
-  description = "Resource group location"
+    type        = string
+    description = "Resource group location"
 }
 
 variable "prefix" {
-  type        = string
-  description = "Prefix for all resources"
+    type        = string
+    description = "Prefix for all resources"
 }
 
 variable "cluster_name" {
-  type        = string
-  description = "Name of the AKS cluster"
+    type        = string
+    description = "Name of the AKS cluster"
 }
 
 variable "default_node_pool_vm_size" {
-  type        = string
-  description = "Default node pool VM size"
+    type        = string
+    description = "Default node pool VM size"
 }
 
 variable "default_node_pool_type" {
-  type        = string
-  description = "Default node pool type"
+    type        = string
+    description = "Default node pool type"
 }
 
 variable "system_node_count" {
-  type        = number
-  description = "Number of system nodes"
+    type        = number
+    description = "Number of system nodes"
 }
 
 variable "acr_name" {
-  type        = string
-  description = "Name of the ACR"
+    type        = string
+    description = "Name of the ACR"
 }
 
 variable "log_analytics_sku" {
-  type        = string
-  description = "Log analytics SKU"
+    type        = string
+    description = "Log analytics SKU"
 }
 
 variable "kubernetes_version" {
-  type        = string
-  description = "Kubernetes version"
+    type        = string
+    description = "Kubernetes version"
 }
 
 variable "should_deploy_acr" {
-  type        = bool
-  description = "Should deploy ACR"
+    type        = bool
+    description = "Should deploy ACR"
 }
 
 variable "should_deploy_log_analytics" {
-  type        = bool
-  description = "Should deploy log analytics"
+    type        = bool
+    description = "Should deploy log analytics"
 }
 
 variable "should_deploy_prometheus" {
-  type        = bool
-  description = "Should deploy prometheus"
+    type        = bool
+    description = "Should deploy prometheus"
+}
+
+variable "subscription_id" {
+    type        = string
+    description = "Azure subscription ID"
+}
+
+variable "tenant_id" {
+    type        = string
+    description = "Azure tenant ID"
+}
+
+variable "client_id" {
+    type        = string
+    description = "Azure client ID"
+}
+
+variable "client_secret" {
+    type        = string
+    description = "Azure client secret"
 }
