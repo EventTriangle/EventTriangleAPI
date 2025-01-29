4a03ab26a98bfda82cf61d8590b02cfc5863002c
kolosovpetro <kolosovp94@gmail.com>
2025-01-29 04:38:07 +0100

    backup bash docker build

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -20,3 +20,4 @@ and this project adheres to [Semantic Versioning v2.0.0](https://semver.org/spec
 - K8s Rollout deploy pipeline deprecated
 - Merge two docker builds templates for docker build and push
 - Add integration tests project to auth service
+- Create a separate powershell script for build and tag docker images
---
+++ scripts/Build-Docker.sh
@@ -0,0 +1,17 @@
+﻿GIT_VERSION_IMAGE="${{ parameters.dockerRegistryUrl }}/${{ parameters.imageRepository }}:$(GitVersion.SemVer)"
+LATEST_VERSION_IMAGE="${{ parameters.dockerRegistryUrl }}/${{ parameters.imageRepository }}:latest"
+echo "GIT_VERSION_IMAGE: $GIT_VERSION_IMAGE"
+echo "LATEST_VERSION_IMAGE: $LATEST_VERSION_IMAGE"
+
+ACR_GIT_VERSION_IMAGE="${{ parameters.acrRegistryUrl }}/${{ parameters.imageRepository }}:$(GitVersion.SemVer)"
+ACR_LATEST_VERSION_IMAGE="${{ parameters.acrRegistryUrl }}/${{ parameters.imageRepository }}:latest"
+echo "ACR_GIT_VERSION_IMAGE: $ACR_GIT_VERSION_IMAGE"
+echo "ACR_LATEST_VERSION_IMAGE: $ACR_LATEST_VERSION_IMAGE"
+
+docker build --build-arg FRONT_API_URL="${{ parameters.dockerBuildParameterUrl }}" \
+            --build-arg VERSION=$(GitVersion.SemVer) -t "$GIT_VERSION_IMAGE" \
+            -f ${{ parameters.dockerfilePath }} .
+
+docker tag "$GIT_VERSION_IMAGE" "$LATEST_VERSION_IMAGE"
+docker tag "$GIT_VERSION_IMAGE" "$ACR_LATEST_VERSION_IMAGE"
+docker tag "$GIT_VERSION_IMAGE" "$ACR_GIT_VERSION_IMAGE"
