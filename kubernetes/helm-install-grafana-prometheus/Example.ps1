7f8a8d99e2809d14fc90ec2d75f38be9b3721447
kolosovpetro <kolosovp94@gmail.com>
2025-03-04 01:45:02 +0100

    infrastructure update

---
+++ kubernetes/helm-install-grafana-prometheus/Example.ps1
@@ -0,0 +1,6 @@
+﻿helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+kubectl create namespace monitoring
+helm install prometheus prometheus-community/kube-prometheus-stack -n monitoring --set grafana.service.type=ClusterIP --set prometheus.service.type=ClusterIP
+kubectl get svc -n monitoring
+
