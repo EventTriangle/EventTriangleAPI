7f7d5189fa68831abd83a0c0595183fcb9d2837a
kolosovpetro <kolosovp94@gmail.com>
2025-03-07 10:58:03 +0100

    redis helm

---
+++ kubernetes/helm-install-redis/Commands.ps1
@@ -0,0 +1,10 @@
+﻿helm upgrade --install event-redis bitnami/redis --namespace event-triangle
+
+# Get the base64 encoded password from the Kubernetes secret
+$encodedPassword = kubectl get secret --namespace event-triangle event-redis -o jsonpath="{.data.redis-password}"
+
+# Decode the base64 string
+$decodedPassword = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($encodedPassword))
+
+# Set the decoded password into the variable
+$REDIS_PASSWORD = $decodedPassword
