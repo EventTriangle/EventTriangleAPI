39a1bd6bfeadf4a4a0e67cc5af98c6e5f3be218d
Petro Kolosov <kolosovp94@gmail.com>
2026-04-14 21:02:38 +0200

    rabbitmq platform

--- platform/helm-install-rabbit-mq/deploy-rabbitmq-example.ps1
+++ platform/helm-install-rabbit-mq/deploy-rabbitmq-example.ps1
@@ -1,21 +1,3 @@
-az aks get-credentials --resource-group $(terraform output -raw rg_name) --name $(terraform output -raw aks_name) --subscription $(terraform output -raw subscription)
+kubectl apply -f "https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml"
 
-helm repo add bitnami https://charts.bitnami.com/bitnami
-
-helm repo update
-
-helm install event-rabbitmq bitnami/rabbitmq `
-  --namespace event-triangle `
-  --set auth.username=guest `
-  --set auth.password=guest `
-  --set service.type=LoadBalancer
-
-helm upgrade event-rabbitmq bitnami/rabbitmq `
-  --set auth.username=guest `
-  --set auth.password=guest
-
-helm uninstall event-rabbitmq
-
-kubectl get endpoints
-
-kubectl describe service "event-rabbitmq"
+kubectl apply -f rabbit.yaml
