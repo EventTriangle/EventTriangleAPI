60b291525878e695dc301e876967e90266379b51
Petro Kolosov <kolosovp94@gmail.com>
2026-04-15 22:55:38 +0200

    configure platform

--- terraform-azdo-libraries/redis-library.tf
+++ terraform-azdo-libraries/redis-library.tf
@@ -1,7 +1,7 @@
 resource "azuredevops_variable_group" "redis_secrets" {
   project_id   = var.project_id
-  name         = "Postgres_Rabbit_Connection_Credentials"
-  description  = "Postgres_Rabbit_Connection_Credentials"
+  name         = "Redis_Credentials"
+  description  = "Redis_Credentials"
   allow_access = true
 
   variable {
